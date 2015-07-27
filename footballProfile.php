69652eeaae8ea6dbe7e9d1e3743c08c0b70d57c7
DouglasWalker <doug@doug-walker.co.uk>
2015-07-27 22:38:52 +0100

    Updated footballProfile.php
    Formatted table.

--- footballProfile.php
+++ footballProfile.php
@@ -4,81 +4,92 @@
 <link rel="stylesheet" type="text/css" href="theme.css">
 <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-<script src="http://code.highcharts.com/highcharts.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
+<script src="http://code.highcharts.com/highcharts.js"></script>
 
 </head>
 
 
 <body>
-<p id="title1">Current Squad List</p>
-
-<p><div id='container' style='width:550px; height:300px;'></div></p>
-
-
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "irene";
-$dbname = "footballdb";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-$sql = "SELECT fp.playerId,s.shirtNumber,c.countryName,c.countryCode,fp.firstName,fp.lastName,CONCAT(fp.firstName,' ',fp.lastName) AS 'fullName',fp.playerImage,ROUND(AVG(mr.playerRating),2) 'rating1',
-GROUP_CONCAT(mr.playerRating,' ') 'recentRatings'
-FROM footballplayers fp
-LEFT JOIN matchRatings mr ON mr.playerId=fp.playerId
-LEFT JOIN shirtNumbers s ON s.playerId=fp.playerId
-LEFT JOIN countries c ON c.countryId=fp.countryId
-GROUP BY fp.playerId
-ORDER BY rating1 DESC" ;
-$result1 = $conn->query($sql);
-
-$ratingPull=array();
-$namesArray=array();
-
-if ($result1->num_rows > 0) {
-    // output data of each row
-    while($row = $result1->fetch_assoc()) {
-
-      $ratingPull[]=$row["rating1"];
-      $namesArray[]=$row["fullName"];
-
-        echo "<p id=\"rcorners\"><table border='1' style='width:100%'>
+<p id="title1">Current Squad Performance Index</p>
+<table>
   <tr>
-    <td>Squad Number: "
-     . $row["shirtNumber"]. "<br>Player Name: "
-     . $row["fullName"].
-     "<br>Country: " . $row['countryName']. " <img src='../img/flags/" . $row['countryCode']. ".png'>
-     <br>Average Rating: "
-     . $row["rating1"]. "<br>Recent Ratings:<br>"
-       . $row["recentRatings"]. "</td>
-    <td><img id='img_blue' src='../img/players/" . $row["playerImage"]. "'><br>
-      </td>
-  </tr>
+    <td>
+
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "irene";
+  $dbname = "footballdb";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+
+  $sql = "SELECT fp.playerId,s.shirtNumber,c.countryName,c.countryCode,fp.firstName,fp.lastName,CONCAT(fp.firstName,' ',fp.lastName) AS 'fullName',fp.playerImage,ROUND(AVG(mr.playerRating),2) 'rating1',
+  GROUP_CONCAT(mr.playerRating,' ') 'recentRatings'
+  FROM footballplayers fp
+  LEFT JOIN matchRatings mr ON mr.playerId=fp.playerId
+  LEFT JOIN shirtNumbers s ON s.playerId=fp.playerId
+  LEFT JOIN countries c ON c.countryId=fp.countryId
+  GROUP BY fp.playerId
+  ORDER BY rating1 DESC" ;
+  $result1 = $conn->query($sql);
+
+  $ratingPull=array();
+  $namesArray=array();
+
+  if ($result1->num_rows > 0) {
+      // output data of each row
+      while($row = $result1->fetch_assoc()) {
+
+        $ratingPull[]=$row["rating1"];
+        $namesArray[]=$row["fullName"];
+
+          echo "<p id=\"rcorners\"><table border='1' style='width:100%'>
+
+        <tr>
+      <td width='300'>Player Name: "
+      . $row["fullName"].
+      "<br><br>Squad Number: "
+       . $row["shirtNumber"]. "<br><br>Country: " . $row['countryName']. " <img src='../img/flags/" . $row['countryCode']. ".png'>
+       <br><br>Average Rating: "
+       . $row["rating1"]. "       </td>
+      <td><img id='img_blue' src='../img/players/" . $row["playerImage"]. "'><br>
+        </td>
+    </tr>
+  </table>
+  ";
+     }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+
+
+  ?>
+  </p>
+
+</td>
+<td>
+
+  <p id='container' style='width:850px; height:700px;'></p>
+
+</td>
+</tr>
 </table>
-";
-   }
-} else {
-    echo "0 results";
-}
-$conn->close();
 
-
-?>
-</p>
 <script>
 $( document ).ready(function() {
   $('#container').highcharts({
       chart: {
-          type: 'bar'
+          type: 'column'
       },
       title: {
           text: 'Ratings'
