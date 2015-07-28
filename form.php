ec1fcf53cc0b98d4df6bbd4d835576ec083ef96c
DouglasWalker <doug@doug-walker.co.uk>
2015-07-28 23:00:34 +0100

    example form insert - my first db insert form
    requires insert.php

---
+++ form.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+
+<link rel="stylesheet" type="text/css" href="theme.css">
+<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="http://code.highcharts.com/highcharts.js"></script>
+
+</head>
+
+
+<body>
+
+  <p>
+
+  <form method="post" action="insert.php" method="post">
+
+  stadium<input type="text" name="stadium" size="30"><br>
+  buildDate<input type="text" name="buildDate" size="30"><br>
+  seatedCapacity<input type="text" name="seatedCapacity" size="30"><br>
+  overallCapacity<input type="text" name="overallCapacity" size="30"><br>
+  <input type="submit" name="submit" value="Sent">
+
+</p>
+
+
+
+
+</body>
+</html>
